d4d5cd8e7796c601450c0a384ac6a5126a316880
le_woudar <kevin.tewouda@gandi.net>
2021-07-31 22:34:18 +0200

    docs: documented path parameter user_id

--- pastebin/dependencies.py
+++ pastebin/dependencies.py
@@ -1,11 +1,13 @@
 import uuid
 
-from fastapi import HTTPException
+from fastapi import HTTPException, Path
 
 from .users.models import User
 
 
-async def get_db_user(user_id: uuid.UUID) -> User:
+async def get_db_user(
+        user_id: uuid.UUID = Path(..., description='user id', example='7fef63f3-c616-4a3b-bc4a-11917a46c5aa')
+) -> User:
     user = await User.filter(pk=str(user_id)).get_or_none()
     if user is None:
         raise HTTPException(status_code=404, detail=f'user with id {user_id} not found')
